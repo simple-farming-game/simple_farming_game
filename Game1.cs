5b63730659d8242b02ba940176e058de958a9d5c
newkincode <94734506+newkincode@users.noreply.github.com>
2022-08-20 14:59:31 +0900

    no message

--- Game1.cs
+++ Game1.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.Tiled;
+using MonoGame.Extended.Tiled.Renderers;
+using MonoGame.Extended;
+using MonoGame.Extended.ViewportAdapters;
 
 namespace simple_farming_game
 {
@@ -10,8 +14,15 @@ namespace simple_farming_game
         Vector2 playerPosition;
         float playerSpeed;
 
+        TiledMap _tiledMap;
+        TiledMapRenderer _tiledMapRenderer;
+
+        private OrthographicCamera _camera;
+
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        private Vector2 _cameraPosition;
 
         public Game1()
         {
@@ -26,6 +37,9 @@ namespace simple_farming_game
             playerPosition = new Vector2(_graphics.PreferredBackBufferWidth / 2,_graphics.PreferredBackBufferHeight / 2);
             playerSpeed = 550f;
 
+            var viewportadapter = new BoxingViewportAdapter(Window, GraphicsDevice, 800, 600);
+            _camera = new OrthographicCamera(viewportadapter);
+
             base.Initialize();
         }
 
@@ -35,6 +49,9 @@ namespace simple_farming_game
 
             // TODO: use this.Content to load your game content here
             playerTexture = Content.Load<Texture2D>("player");
+
+            _tiledMap = Content.Load<TiledMap>("map");
+            _tiledMapRenderer = new TiledMapRenderer(GraphicsDevice, _tiledMap);
         }
 
         protected override void Update(GameTime gameTime)
@@ -65,6 +82,11 @@ namespace simple_farming_game
                 playerPosition.X += playerSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
             }
 
+            _tiledMapRenderer.Update(gameTime);
+
+            MoveCamera(gameTime);
+            _camera.LookAt(_cameraPosition);
+
             base.Update(gameTime);
         }
 
@@ -87,7 +109,48 @@ namespace simple_farming_game
             );
             _spriteBatch.End();
 
+            _tiledMapRenderer.Draw();
+
+            _tiledMapRenderer.Draw(_camera.GetViewMatrix());
+
             base.Draw(gameTime);
+        }
+        private Vector2 GetMovementDirection()
+        {
+            var movementDirection = Vector2.Zero;
+            var state = Keyboard.GetState();
+            if (state.IsKeyDown(Keys.Down))
+            {
+                movementDirection += Vector2.UnitY;
+            }
+            if (state.IsKeyDown(Keys.Up))
+            {
+                movementDirection -= Vector2.UnitY;
+            }
+            if (state.IsKeyDown(Keys.Left))
+            {
+                movementDirection -= Vector2.UnitX;
+            }
+            if (state.IsKeyDown(Keys.Right))
+            {
+                movementDirection += Vector2.UnitX;
+            }
+
+            // Can't normalize the zero vector so test for it before normalizing
+            if (movementDirection != Vector2.Zero)
+            {
+                movementDirection.Normalize();
+            }
+
+            return movementDirection;
+        }
+
+        private void MoveCamera(GameTime gameTime)
+        {
+            var speed = 200;
+            var seconds = gameTime.GetElapsedSeconds();
+            var movementDirection = GetMovementDirection();
+            _cameraPosition += speed * movementDirection * seconds;
         }
     }
 }
