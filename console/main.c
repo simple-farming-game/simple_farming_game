97f27e7d79cefbaca80f05df0b6685543059ab5f
newkincode <94734506+newkincode@users.noreply.github.com>
2022-10-17 16:01:29 +0900

    Update main.c

--- console/main.c
+++ console/main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
+void gotoxy(int x, int y){
+	COORD pos = {x,y};
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+
 int main(){
+
+    gotoxy(100, 100);
     printf("hello \n");
 
     return 0;
