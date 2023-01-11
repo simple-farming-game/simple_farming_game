347b3694ee7836e511d5111a17bdd01cd3dcd76e
DESKTOP-H9OD0J1\gongb <maainnewkin59@gmail.com>
2023-01-11 20:41:07 +0900

    z

---
+++ 빠람빰빰/main.py
@@ -0,0 +1,17 @@
+import os
+import time
+print(59339)
+os.system("cls")
+print("시작부터 퀴즈임\n내가 만들고 있는 게임의 이름은?")
+print("1. farmProject")
+print("2. farmgame")
+print("3. sfg")
+a = int(input())
+if a == 1:
+    pass
+elif a == 2:
+    pass
+elif a ==3:
+    print("R0VZREFPST0=")#base
+
+#우와 이거 봤어? 대단하다... 그러면... https://github.com/newkincode/barambambam
---
+++ 빠람빰빰/새 폴더/1009.py
@@ -0,0 +1,5 @@
+a = int(input())
+if a == 14339:
+    print("뭐야 어캐 찾음? 39200")
+import os
+os.system("cls")
