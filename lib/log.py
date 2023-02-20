6a80acabe85760004e18938f9a788e5156bd1e82
Gongback <94734506+newkincode@users.noreply.github.com>
2023-02-20 20:30:11 +0900

    그래... 아직 내가할께 아니야...

--- lib/log.py
+++ lib/log.py
@@ -5,24 +5,25 @@ now.strftime('%Y-%m-%d %H:%M:%S')
 class log:
     def __init__(self):
         self.now = datetime.datetime.now()
+        self.nows = datetime.datetime.now()
         self.log = []
     def custem(self,text):
         self.log.append(f"{text}")
         print(self.log[len(self.log)-1])
     def debug(self,text):
-        self.log.append(f"{self.now.strftime('%Y-%m-%d %H-%M-%S')} - DEBUG - {text}")
+        self.log.append(f"{self.nows.strftime('%Y-%m-%d %H-%M-%S')} - DEBUG - {text}")
         print(self.log[len(self.log)-1])
     def info(self,text):
-        self.log.append(f"{self.now.strftime('%Y-%m-%d %H-%M-%S')} - INFO - {text}")
+        self.log.append(f"{self.nows.strftime('%Y-%m-%d %H-%M-%S')} - INFO - {text}")
         print(self.log[len(self.log)-1])
     def warning(self,text):
-        self.log.append(f"{self.now.strftime('%Y-%m-%d %H-%M-%S')} - WARNING - {text}")
+        self.log.append(f"{self.nows.strftime('%Y-%m-%d %H-%M-%S')} - WARNING - {text}")
         print(self.log[len(self.log)-1])
     def error(self,text):
-        self.log.append(f"{self.now.strftime('%Y-%m-%d %H-%M-%S')} - ERROR - {text}")
+        self.log.append(f"{self.nows.strftime('%Y-%m-%d %H-%M-%S')} - ERROR - {text}")
         print(self.log[len(self.log)-1])
     def critical(self,text):
-        self.log.append(f"{self.now.strftime('%Y-%m-%d %H-%M-%S')} - CRITICAL - {text}")
+        self.log.append(f"{self.nows.strftime('%Y-%m-%d %H-%M-%S')} - CRITICAL - {text}")
         print(self.log[len(self.log)-1])
     def save(self):
         try:
@@ -36,7 +37,4 @@ class log:
             for i in self.log:
                 f.write(f"{i}\n")
             f.close
-logs = log()
-logs.debug("helo")
-logs.debug("helo")
-logs.save()
+        self.nows = datetime.datetime.now()
