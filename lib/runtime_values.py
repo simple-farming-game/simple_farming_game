f739542049dedbc9ad91416808a8f03197791b5d
newkincode <maainnewkin59@gmail.com>
2023-03-15 22:09:57 +0900

    영어추가, 버전수정

--- lib/runtime_values.py
+++ lib/runtime_values.py
@@ -8,7 +8,7 @@ from lib.logger import logger
 from lib import player
 
 version_type = Tuple[str, int, int, int]
-version: version_type = ("devalpha", 2, 0, 5)
+version: version_type = ("devalpha", 2, 1, 0)
 
 comInfo = {
     "core": os.cpu_count(),
