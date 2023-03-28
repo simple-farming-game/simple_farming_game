569feabb757469a969e65db2641ed0f9d96ef731
newkincode <maainnewkin59@gmail.com>
2023-03-29 08:01:06 +0900

    해결했지만 에러

--- lib/save.py
+++ lib/save.py
@@ -9,9 +9,9 @@ from lib import runtime_values
 def write_save():
     with open("save.sfgsave", "w") as save:
         save.write(json.dumps({
-            "version": "runtime_values.version",
-            "tile": "farm.tileMap",
-            "player_data": "runtime_values.players"
+            "version": f"{runtime_values.version}",
+            "tile": f"{farm.tileMap}",
+            "player_data": f"{runtime_values.players}"
         }))
     runtime_values.logs.info("저장")
 
@@ -22,31 +22,26 @@ def import_save() -> bool:
     try:
         with open("save.sfgsave", "r") as saveFile:
             saveData = json.load(saveFile)
+            
     except:
         runtime_values.logs.info("불러오기 실패: 파일 불러오기 실패.")
         return False
 
     # check version
     version: runtime_values.version_type
-    try:
-        version = saveData["version"]
-    except KeyError:
-        return import_legacy_save(("non", 0, 0, 0), saveData)
-    if runtime_values.version != version:
-        return import_legacy_save(version, saveData)
+    version =  tuple(saveData["version"])
+    print(runtime_values.version != version)
 
     # alpha 2.0.0
     farm.tileMap = saveData["tile"]
     runtime_values.players = saveData["player_data"]
+    runtime_values.logs.info("성공!")
     return True
 
 
 def import_legacy_save(version: Tuple[str, int, int, int], saveData) -> bool:
     if version == ("non", 0, 0, 0):
         # alpha 1.1.1
-        exec("%s = %d" % (saveData["inv"],saveData["inv"]))
-        exec("%s = %d" % (saveData["tile"],saveData["tile"]))
-        exec("%s = %d" % (saveData["pos"],saveData["pos"]))
         runtime_values.players[0].inventory = saveData["inv"]
         farm.tileMap = saveData["tile"]
         runtime_values.players[0].pos = saveData["pos"]
