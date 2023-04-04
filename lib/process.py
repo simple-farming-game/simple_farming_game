6ae00ea52ef8b7ff907177423e82c106d633ec2d
newkincode <maainnewkin59@gmail.com>
2023-04-04 22:10:48 +0900

    아이 되는줄 알았네

--- lib/process.py
+++ lib/process.py
@@ -1,8 +1,13 @@
 from lib import farm
 from lib.block import block_list
+import pygame
 
 def process():
-    for i in farm.tileMap:
-        for j in i:
-            if j in block_list.block_list:
-                j.water()
+    tilePos = pygame.math.Vector2(0, 0)
+    for line in farm.tileMap:
+        for tile in line:
+            if isinstance(tile, block_list.block_list):  # type: ignore
+                tile.water() # type: ignore
+            tilePos.y += 32
+        tilePos.x += 32
+        tilePos.y = 0
