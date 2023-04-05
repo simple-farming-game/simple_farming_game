0b39731879921b27607a0c93ac3b9f054704c167
newkincode <maainnewkin59@gmail.com>
2023-04-05 22:08:49 +0900

    태스트 코드 삭제

--- lib/block/sprinkle.py
+++ lib/block/sprinkle.py
@@ -18,10 +18,8 @@ class Sprinkle(Block.Block):
         self.rangeList = [self.rangeList[i:i+2] for i in range(0, len(self.rangeList), 2)]
                 
     
-    def water(self, screen):
+    def water(self):
         for i in self.rangeList:
-            pygame.draw.rect(screen,(0,0,0),(i[0]*32,i[1]*32,10,10))
             if isinstance(farm.tileMap[i[0]][i[1]], plants_list.plants_type):
-                print(farm.tileMap[i[0]][i[1]])
                 farm.tileMap[i[0]][i[1]].water = True # type: ignore
             
