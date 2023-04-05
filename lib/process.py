c085779c94bb3257574d282feabe73b3ee9b9584
newkincode <maainnewkin59@gmail.com>
2023-04-05 22:07:53 +0900

    기능완성

--- lib/process.py
+++ lib/process.py
@@ -1,13 +1,14 @@
 from lib import farm
 from lib.block import block_list
 import pygame
+import lib.runtime_values as rv
 
 def process():
     tilePos = pygame.math.Vector2(0, 0)
     for line in farm.tileMap:
         for tile in line:
-            if isinstance(tile, block_list.block_list):  # type: ignore
-                tile.water() # type: ignore
+            if isinstance(tile, block_list.block_list[1]):  # type: ignore
+                tile.water(rv.screen) # type: ignore
             tilePos.y += 32
         tilePos.x += 32
         tilePos.y = 0
