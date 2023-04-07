2f5a0be084ec629a50416beb28af301bc8fd9823
newkincode <maainnewkin59@gmail.com>
2023-04-07 15:39:30 +0900

    no message

--- lib/block/Block.py
+++ lib/block/Block.py
@@ -5,6 +5,7 @@ class Block(Object):
     name: str  # 영어로 적고 lang 파일에서 참조.
 
     def __init__(self, pos: pygame.math.Vector2, screen: pygame.Surface) -> None:
+<<<<<<< HEAD
         self.image = pygame.image.load(f"assets/img/block/{self.name}.png")
         self.pos = pos
         self.screen = screen
@@ -13,3 +14,7 @@ class Block(Object):
     def returnVar(self):
         return [self.image, self.pos, self.screen]
     def init(self): ...
+=======
+        image = pygame.image.load(f"assets/img/block/{self.name}.png")
+        super().__init__(image, pos, screen)
+>>>>>>> d6beeeb (블록 놓기 완성 하지만 오류)
