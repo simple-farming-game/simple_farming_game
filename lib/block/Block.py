a88700d457fd3a5c175e29076d2aa01b4bf2daac
newkincode <maainnewkin59@gmail.com>
2023-04-09 21:05:55 +0900

    Revert "no message"
    This reverts commit a2ef3cd11b9186d8d7aae9bc3d01cf63b2f0e460.

---
+++ lib/block/Block.py
@@ -0,0 +1,15 @@
+from ..Object import Object
+import pygame
+
+class Block(Object):
+    name: str  # 영어로 적고 lang 파일에서 참조.
+
+    def __init__(self, pos: pygame.math.Vector2, screen: pygame.Surface) -> None:
+        self.image = pygame.image.load(f"assets/img/block/{self.name}.png")
+        self.pos = pos
+        self.screen = screen
+        self.init()
+        super().__init__(self.image, pos, screen)
+    def returnVar(self):
+        return [self.image, self.pos, self.screen]
+    def init(self): ...
--- lib/draw.py
+++ lib/draw.py
@@ -3,6 +3,7 @@ import pygame
 from lib.runtime_values import players
 from lib import farm
 from lib.plants import plants_list
+from lib.block import block_list
 
 
 ground_images: dict[farm.Tiles, pygame.Surface] = {
@@ -16,16 +17,16 @@ def draw_ground(screen: pygame.Surface):
     tilePos = pygame.math.Vector2(0, 0)
     for line in farm.tileMap:
         for tile in line:
-<<<<<<< HEAD
             if tile in plants_list.plants_list and not tile.water:
-=======
-            if isinstance(tile, plants_list.plants_list) and not tile.water:  # type: ignore
->>>>>>> parent of f06ee65 (Merge branch 'main' into dev/opning)
                 screen.blit(ground_images[farm.Tiles.FARMLAND], tilePos)
             elif isinstance(tile, plants_list.plants_list) and tile.water:  # type: ignore
                 screen.blit(ground_images[farm.Tiles.WATER_FARMLAND], tilePos)
-            elif tile in farm.Tiles:
-                screen.blit(ground_images[tile], tilePos)  # type: ignore
+            elif not tile in plants_list.plants_list:
+                try:
+                    if tile in farm.Tiles:
+                        screen.blit(ground_images[tile], tilePos)  # type: ignore
+                except:
+                    draw_block(screen, tilePos)
             tilePos.y += 32
         tilePos.x += 32
         tilePos.y = 0
@@ -36,8 +37,13 @@ def draw_plants():
         for tile in line:
             if isinstance(tile, plants_list.plants_list):  # type: ignore
                 tile.draw()  # type: ignore
-            if isinstance(tile, plants_list.plants_list):  # type: ignore
+def draw_block(screen, tilePos):
+    for line in farm.tileMap:
+        for tile in line:
+            if isinstance(tile, block_list.block_list):  # type: ignore
+                screen.blit(ground_images[farm.Tiles.DIRT], tilePos)
                 tile.draw()  # type: ignore
+                
 
 def draw_players():
     for player in players:
@@ -45,7 +51,6 @@ def draw_players():
 
 
 def draw_text_with_border(screen: pygame.Surface, font: pygame.font.Font, text: str, inside_color: pygame.Color, border_color: pygame.Color, border_size: float, positon: pygame.math.Vector2):
-    # logs.debug("twb")
     inside = font.render(text, True, inside_color)
     border = font.render(text, True, border_color)
 
