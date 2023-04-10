64f31989e128cd7ca8012b6d783c2a54d4fbab2c
newkincode <maainnewkin59@gmail.com>
2023-04-10 23:54:43 +0900

    버그해결

--- lib/draw.py
+++ lib/draw.py
@@ -17,7 +17,7 @@ def draw_ground(screen: pygame.Surface):
     tilePos = pygame.math.Vector2(0, 0)
     for line in farm.tileMap:
         for tile in line:
-            if isinstance(tile, plants_list.plants_list): # type: ignore
+            if isinstance(tile, plants_list.plants_list) and not tile.water: # type: ignore
                 screen.blit(ground_images[farm.Tiles.FARMLAND], tilePos)
             elif isinstance(tile, plants_list.plants_list) and tile.water:  # type: ignore
                 screen.blit(ground_images[farm.Tiles.WATER_FARMLAND], tilePos)
