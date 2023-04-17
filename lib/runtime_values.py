9b17af045acdf3c3ec6c3ba48d6579ef7cab7ac8
newkincode <maainnewkin59@gmail.com>
2023-04-17 19:39:54 +0900

    버전수정

--- lib/runtime_values.py
+++ lib/runtime_values.py
@@ -8,7 +8,7 @@ from lib.logger import logger
 from lib import player
 
 version_type = Tuple[str, int, int, int]
-version: version_type = ("alpha", 2, 1, 11)
+version: version_type = ("alpha", 2, 1, 12)
 
 pcInfo = {
     "core": os.cpu_count(),
