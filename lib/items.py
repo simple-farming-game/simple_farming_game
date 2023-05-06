0c8bf1fa925e2e7e2264b77ee61ba00c7dcbb54a
newkincode <maainnewkin59@gmail.com>
2023-05-06 20:43:12 +0900

    음... enum을 안쓰면 안되는구나...

--- lib/items.py
+++ lib/items.py
@@ -3,16 +3,17 @@ from lib.plants import plants_list
 from lib.block import block_list
 
 
-class Items(Enum):
+class Items:
     HOE = auto()
     SHOVEL = auto()
     SICKLE = auto()
     VITAMIN = auto()
     WATER = auto()
     NONE = auto()
-
+    
 for i in plants_list.plants_list:
-    setattr(Items, i.name.upper(), auto())
-
+    exec(f"Items.{i.name.upper()} = auto()")
 for i in block_list.block_list:
-    setattr(Items, i.name.upper(), auto())
+    exec(f"Items.{i.name.upper()} = auto()")
+
+print(list(Items.__dict__.keys()))
