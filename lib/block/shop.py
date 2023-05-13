1bfedbb75a4efdd5a72ba75bc422f7434328f51e
newkincode <maainnewkin59@gmail.com>
2023-05-13 22:43:56 +0900

    add - shop open bug - shop not opne

--- lib/block/shop.py
+++ lib/block/shop.py
@@ -2,8 +2,6 @@ import pygame
 import random
 
 from . import Block
-from .. import farm
-from ..plants import plants_list
 
 class Shop(Block.Block):
     is_shop_opne: bool = False
@@ -14,7 +12,22 @@ class Shop(Block.Block):
     
     def interact(self):
         self.is_shop_opne = not self.is_shop_opne
-    
+        
+    @staticmethod
+    def draw_text_with_border(screen: pygame.Surface, font: pygame.font.Font, text: str, inside_color: pygame.Color, border_color: pygame.Color, border_size: float, positon: pygame.math.Vector2):
+        inside = font.render(text, True, inside_color)
+        border = font.render(text, True, border_color)
+
+        screen.blit(border, pygame.math.Vector2(positon.x-border_size, positon.y))
+        screen.blit(border, pygame.math.Vector2(positon.x+border_size, positon.y))
+        screen.blit(border, pygame.math.Vector2(positon.x, positon.y-border_size))
+        screen.blit(border, pygame.math.Vector2(positon.x, positon.y+border_size))
+        screen.blit(inside, positon)
+
     def update(self):
+        from .. import runtime_values
         if self.is_shop_opne:
-            print("test")
+            self.draw_text_with_border(
+                runtime_values.screen, runtime_values.font,
+                "상점",
+                runtime_values.WHITE, runtime_values.BLACK, 2, pygame.math.Vector2(10, 85))
