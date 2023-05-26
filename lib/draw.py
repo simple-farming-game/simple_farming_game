dc3dfc1184d1d52326852d06b188cbaedc94b907
newkincode <maainnewkin59@gmail.com>
2023-05-26 22:16:21 +0900

    여러 이모지를 추가하기 쉽게 변경

--- lib/draw.py
+++ lib/draw.py
@@ -51,19 +51,21 @@ def draw_players():
     for player in players:
         player.draw()
 
+emoji_list = ["smile"]
 
 def draw_text_with_border(screen: pygame.Surface, font: pygame.font.Font, text: str, inside_color: pygame.Color, border_color: pygame.Color, border_size: float, positon: pygame.math.Vector2):
-    smile_msg_pos = text.find("<smile>")
-    new_text = text.replace("<smile>", "  ")
+    for i in emoji_list:
+        smile_msg_pos = text.find(f"<{i}>")
+        new_text = text.replace(f"<{i}>", "  ")
 
-    inside = font.render(new_text, True, inside_color)
-    border = font.render(new_text, True, border_color)
-    no_smile_one = font.render(text.split("<smile>")[0], True, inside_color)
+        inside = font.render(new_text, True, inside_color)
+        border = font.render(new_text, True, border_color)
+        no_smile_one = font.render(text.split(f"<{i}>")[0], True, inside_color)
 
-    smile_pos = positon.x + no_smile_one.get_width()
+        smile_pos = positon.x + no_smile_one.get_width()
 
-    if smile_msg_pos != -1:
-        screen.blit(imgs.emojis["smile"], [smile_pos, positon.y])
+        if smile_msg_pos != -1:
+            screen.blit(imgs.emojis[f"<{i}>"], [smile_pos, positon.y])
 
     screen.blit(border, pygame.math.Vector2(positon.x-border_size, positon.y))
     screen.blit(border, pygame.math.Vector2(positon.x+border_size, positon.y))
