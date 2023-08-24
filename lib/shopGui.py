bec823cd5636b3a4dc4f6b488caf568a079a5d8a
newkincode <maainnewkin59@gmail.com>
2023-08-24 21:18:47 +0900

    feat : 상점 페이지 제작

--- lib/shopGui.py
+++ lib/shopGui.py
@@ -5,16 +5,16 @@ btn_list = []
 def init(plants_list, block_list):
     from lib.lang import text
     from lib import ui
-    btn_y = 0
+    btn_y = 10
     index = 1
     for item in plants_list:
-        btn_list.append(ui.Btn(f"{index}. {text(f'items.{item.name}')}", lambda: buy(item.name), pygame.math.Vector2(50,btn_y)))
+        btn_list.append(ui.Btn(f"{index}. {text(f'items.{item.name}')}", lambda: buy(item.name), pygame.math.Vector2(10,btn_y)))
         index+=1
-        btn_y+=50
+        btn_y+=30
     for item in block_list:
-        btn_list.append(ui.Btn(f"{index}. {text(f'blocks.{item.name}')}", lambda: buy(item.name), pygame.math.Vector2(50,btn_y)))
+        btn_list.append(ui.Btn(f"{index}. {text(f'blocks.{item.name}')}", lambda: buy(item.name), pygame.math.Vector2(10,btn_y)))
         index+=1
-        btn_y+=50
+        btn_y+=30
 
 def buy(name):
     from lib import sell
@@ -22,5 +22,8 @@ def buy(name):
     sell.buy(name)
 
 def shop_open():
+    from lib.new_screen import background
+    from lib.new_screen import color
+    background(color.SKYBLUE)
     for btn in btn_list:
         btn.draw()
