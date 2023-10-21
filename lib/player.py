d9e971f4d988032e12ab558cce3079549f86715c
newkincode <maainnewkin59@gmail.com>
2023-10-21 22:25:57 +0900

    Revert "del : 스테이시 삭제용 커밋"
    This reverts commit e420dc1c06a8c801b2c88e983822d85864d91cac.

--- lib/player.py
+++ lib/player.py
@@ -85,15 +85,7 @@ class player(Object):
 
     def get_tile_pos(self) -> pygame.math.Vector2:
 <<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
         return self.pos // 32
-=======
-        return self.pos//32
->>>>>>> Stashed changes
-=======
-        return self.pos//32
->>>>>>> Stashed changes
 =======
         return self.pos//32
 >>>>>>> Stashed changes
