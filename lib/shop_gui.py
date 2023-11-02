519aca6b20d9ba3c9c33fd651720748738aa1cba
newkincode <maainnewkin59@gmail.com>
2023-11-02 19:29:23 +0900

    faet : 상점에 돈 추가

--- lib/shop_gui.py
+++ lib/shop_gui.py
@@ -24,6 +24,26 @@ def buy(name):
 def shop_open():
     from lib.new_screen import background
     from lib.new_screen import color
+    from lib import draw
+    from lib.lang import text
+    from lib import runtime_values
+    
+    font_renderer = runtime_values.font
+    SKYBLUE = pygame.Color(113, 199, 245)
+    BLACK = pygame.Color(0, 0, 0)
+    WHITE = pygame.Color(255, 255, 255)
+
+    
     background(color.SKYBLUE)
+    draw.draw_text_with_border(  # 좌표
+                runtime_values.screen,
+                font_renderer,
+                f"{text(f'gold')} : {runtime_values.players[0].gold}",
+                WHITE,
+                BLACK,
+                2,
+                pygame.math.Vector2(5,runtime_values.window_size[1]-25),
+            )
+    
     for btn in btn_list:
         btn.draw()
