3c40b5c826ab07635990de499925ffa5be921b1e
newkincode <maainnewkin59@gmail.com>
2023-12-24 15:05:04 +0900

    add version

--- lib/runtime_values.py
+++ lib/runtime_values.py
@@ -4,10 +4,12 @@ from lib.log4py import Logger
 # Var
 screen_size: tuple = (960, 640)
 is_running: bool = True
-logger = Logger()
+logger: Logger = Logger()
+version: tuple[int, int, int, str] = (0, 2, 0, "alpha")
+ver_text = f"{version[0]}.{version[1]}.{version[2]} {version[3]}"
 
 # Color
-SKYBLUE = pygame.Color(113, 199, 245)
+SKYBLUE:pygame.Color = pygame.Color(113, 199, 245)
 
 # Pygame Var
 screen: pygame.Surface = pygame.display.set_mode(screen_size)
