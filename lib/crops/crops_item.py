d5b8a41562355be211c7d5c0e90b76dbccab00a1
newkincode <maainnewkin59@gmail.com>
2025-01-11 14:22:38 +0900

    test

--- lib/crops/crops_item.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum, auto
-from lib.crops.rice import Rice
-
-class CropsItems(Enum):
-    RICE = Rice
-
-crops_item_name_list = [item.name for item in CropsItems]
-crops_item_name_list_lower = [item.name.lower() for item in CropsItems]
