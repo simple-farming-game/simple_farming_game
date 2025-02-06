4d36f86357a2ff55e4da9ba1e54eb60ac527fc86
newkincode <maainnewkin59@gmail.com>
2025-02-06 18:02:07 +0900

    BugFix run java on Windows

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 call gradlew.bat jar
 rd /s /q build\libs\resources
 xcopy /e /i /h src\main\resources build\libs\resources
-java -XstartOnFirstThread -jar build\libs\sfg-1.0-SNAPSHOT-all.jar
+java -jar build\libs\sfg-1.0-SNAPSHOT-all.jar
